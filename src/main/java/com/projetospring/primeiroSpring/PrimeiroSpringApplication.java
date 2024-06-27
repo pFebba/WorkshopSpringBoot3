03b4d4bb338055ac1122e923f3b45da4f016cb53
pFebba <pedro.febba@outlook.com>
2024-06-27 20:04:55 -0300

    Projeto criado

---
+++ src/main/java/com/projetospring/primeiroSpring/PrimeiroSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projetospring.primeiroSpring;
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroSpringApplication.class, args);
+	}
+
+}
