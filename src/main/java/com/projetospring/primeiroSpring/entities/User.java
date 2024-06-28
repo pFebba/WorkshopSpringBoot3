93655ca6e02faa5e37da13d189c884178043fd3b
pFebba <pedro.febba@outlook.com>
2024-06-28 13:05:47 -0300

    Adicionada classe 'Order' e feita associação com a classe 'User'

--- src/main/java/com/projetospring/primeiroSpring/entities/User.java
+++ src/main/java/com/projetospring/primeiroSpring/entities/User.java
@@ -1,12 +1,15 @@
 package com.projetospring.primeiroSpring.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 //Annotation que define a classe User como uma entidade
@@ -28,6 +31,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy  = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -79,6 +85,11 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
--- src/main/java/com/projetospring/primeiroSpring/services/UserService.java
+++ src/main/java/com/projetospring/primeiroSpring/services/UserService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 import com.projetospring.primeiroSpring.entities.User;
 import com.projetospring.primeiroSpring.repositories.UserRepository;
