72fc3e54f9ec433689985b3546deca510039cbd7
pFebba <pedro.febba@outlook.com>
2024-06-29 17:28:14 -0300

    Adicionadas exceções para os métodos 'delete' e 'update'

--- src/main/java/com/projetospring/primeiroSpring/services/UserService.java
+++ src/main/java/com/projetospring/primeiroSpring/services/UserService.java
@@ -1,60 +1,67 @@
 package com.projetospring.primeiroSpring.services;
 
-import java.util.List;
+import java.util.List; 
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-
 
 import com.projetospring.primeiroSpring.entities.User;
 import com.projetospring.primeiroSpring.repositories.UserRepository;
 import com.projetospring.primeiroSpring.services.exceptions.DatabaseException;
 import com.projetospring.primeiroSpring.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		//Optional pode se considerar um contêiner 
-		//de valor único que, ou contém um valor ou não contém
+		// Optional pode se considerar um contêiner
+		// de valor único que, ou contém um valor ou não contém
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
-	public void delete(Long id){
-	    try {
-	        if(!userRepository.existsById(id)) 
-	        	throw new ResourceNotFoundException(id);
-	        userRepository.deleteById(id);
-	    }catch (ResourceNotFoundException e){
-	        throw new ResourceNotFoundException(id);
-	    }
+
+	public void delete(Long id) {
+		try {
+			if (!userRepository.existsById(id))
+				throw new ResourceNotFoundException(id);
+			userRepository.deleteById(id);
+		} catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
+		try {
 		User entity = userRepository.getReferenceById(id);
 		updateData(entity,obj);
 		return userRepository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
+
 }
